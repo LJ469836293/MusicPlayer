afeee7d71cd401ce380d19579c1f3a6702415716
liujing <1010101010@qq.com>
2015-11-01 15:21:57 +0800

    初始化项目

---
+++ 刘静的音乐播放器/刘静的音乐播放器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  刘静的音乐播放器
+//
+//  Created by 刘静 on 15/11/1.
+//  Copyright © 2015年 liujing.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 刘静的音乐播放器/刘静的音乐播放器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  刘静的音乐播放器
+//
+//  Created by 刘静 on 15/11/1.
+//  Copyright © 2015年 liujing.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
