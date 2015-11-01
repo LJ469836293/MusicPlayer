bd722608b1c37c85b87dff2b3b4b5eac46a4f722
liujing <1010101010@qq.com>
2015-11-01 20:10:26 +0800

    初始化项目

---
+++ 01-1刘静的音乐/01-1刘静的音乐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-1刘静的音乐
+//
+//  Created by 刘静 on 15/11/1.
+//  Copyright © 2015年 liujing.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
